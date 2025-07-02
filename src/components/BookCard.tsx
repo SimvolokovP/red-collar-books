b59cf1e450fc5b946829b7c816dd4688b87b46da
Simvolokov Pavel <psimvolokov@gmail.com>
2025-07-02 18:31:55 +0300

    add storage for favorite books & like toggle button

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,14 +1,38 @@
 import { Link } from "react-router-dom";
 import { getShortText } from "../helpers/getShortText";
 import type { Book } from "../models/booksModels";
+import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
+import { useFavoriteBooksStore } from "../store/useFavoriteBooksStore";
 
 interface BookCardProps {
   book: Book;
 }
 
 export function BookCard({ book }: BookCardProps) {
+  const { isFavorite, addToFavorites, removeFromFavorites } =
+    useFavoriteBooksStore();
+  const bookIsFavorite = isFavorite(book.id);
+
+  const handleClick = () => {
+    if (bookIsFavorite) {
+      removeFromFavorites(book.id);
+    } else {
+      addToFavorites(book);
+    }
+  };
+
   return (
-    <article className="bg-secondary p-[8px] rounded-xl h-full shadow flex flex-col items-center">
+    <article className="relative bg-secondary p-[8px] rounded-xl h-full shadow flex flex-col items-center">
+      <button
+        onClick={handleClick}
+        className="absolute top-[8px] right-[8px] cursor-pointer border-1 rounded-full p-1"
+      >
+        {bookIsFavorite ? (
+          <IoMdHeart className="rotate" size={18} />
+        ) : (
+          <IoMdHeartEmpty className="rotate" size={18} />
+        )}
+      </button>
       <h3 className="w-full text-left mb-[16px]">{book.volumeInfo.title}</h3>
       <img
         className="boxShadow rounded-xl h-[180px] mb-[16px]"
@@ -23,7 +47,9 @@ export function BookCard({ book }: BookCardProps) {
       <p className="w-full text-left">
         {book.volumeInfo.description &&
           getShortText(book.volumeInfo.description, 72)}
-        <Link className="font-bold" to={`/book/${book.id}`}>Подробнее</Link>
+        <Link className="font-bold" to={`/book/${book.id}`}>
+          Подробнее
+        </Link>
       </p>
     </article>
   );
---
+++ src/store/useFavoriteBooksStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import type { Book } from "../models/booksModels";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface FavoriteBooksState {
+  favoriteBooks: Book[];
+  addToFavorites: (book: Book) => void;
+  removeFromFavorites: (bookId: string) => void;
+  isFavorite: (bookId: string) => boolean;
+}
+
+export const useFavoriteBooksStore = create<FavoriteBooksState>()(
+  persist(
+    (set, get) => ({
+      favoriteBooks: [],
+      addToFavorites: (book) =>
+        set((state) => ({
+          favoriteBooks: [...state.favoriteBooks, book],
+        })),
+      removeFromFavorites: (bookId) =>
+        set((state) => ({
+          favoriteBooks: state.favoriteBooks.filter(
+            (book) => book.id !== bookId
+          ),
+        })),
+      isFavorite: (bookId: string) =>
+        get().favoriteBooks.some((book) => book.id === bookId),
+    }),
+    {
+      name: "favoriteBooks",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
