251d03d3d324ea933d0d94fd4ac0eb138cafc89e
Simvolokov Pavel <psimvolokov@gmail.com>
2025-07-04 15:24:11 +0300

    edit book card styles

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -19,15 +19,21 @@ export function BookCard({ book }: BookCardProps) {
   const handleClick = () => {
     if (bookIsFavorite) {
       removeFromFavorites(book.id);
-      showToast(`Книга "${book.volumeInfo.title}" удалена из избранного`, "success");
+      showToast(
+        `Книга "${book.volumeInfo.title}" удалена из избранного`,
+        "success"
+      );
     } else {
       addToFavorites(book);
-      showToast(`Книга "${book.volumeInfo.title}" добавлена в избранное`, "success");
+      showToast(
+        `Книга "${book.volumeInfo.title}" добавлена в избранное`,
+        "success"
+      );
     }
   };
 
   return (
-    <article className="relative bg-secondary p-[8px] rounded-xl h-full shadow flex flex-col items-center">
+    <article className="relative bg-secondary p-[8px] rounded-xl h-full shadow flex flex-col">
       <button
         onClick={handleClick}
         className="absolute top-[8px] right-[8px] cursor-pointer border-1 rounded-full p-1 bg-secondary"
@@ -38,20 +44,27 @@ export function BookCard({ book }: BookCardProps) {
           <IoMdHeartEmpty size={18} />
         )}
       </button>
-      <h3 className="w-full text-left mb-[16px]">{book.volumeInfo.title}</h3>
-      <img
-        className="boxShadow rounded-xl h-[180px] mb-[16px]"
-        src={
-          book.volumeInfo.imageLinks?.smallThumbnail || "/book-placeholder.jpg"
-        }
-        alt={book.volumeInfo.title}
-      />
-      <ul className="w-full text-left mb-[8px]">
+      <h3 className="w-full text-left mb-[16px] max-w-[270px]">
+        {book.volumeInfo.title}
+      </h3>
+      <div className="flex w-full justify-center">
+        <img
+          className="boxShadow rounded-xl h-[180px] mb-[16px]"
+          src={
+            book.volumeInfo.imageLinks?.smallThumbnail ||
+            "/book-placeholder.jpg"
+          }
+          alt={book.volumeInfo.title}
+        />
+      </div>
+      <ul className="w-full text-left mb-[8px] flex flex-wrap gap-[8px]">
         {book.volumeInfo.authors?.map((author) => (
-          <li key={author}>{author}</li>
+          <li className="underline" key={author}>
+            {author}
+          </li>
         ))}
       </ul>
-      <p className="w-full text-left">
+      <p className="w-full text-left text-[15px]">
         {book.volumeInfo.description &&
           getShortText(book.volumeInfo.description, 72)}
         <Link className="font-bold" to={`/book/${book.id}`}>
